fedf6c74f9243cf04b02c7dc93813d3bc456477e
Katelyn Yaeger <knyaeger93@gmail.com>
2023-01-17 17:29:04 -0600

    initial RonKanye Commit

--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace APIsAndJSON
 {
     internal class RonVSKanyeAPI
     {
+        public static string KanyeQuote()
+        {
+            var client = new HttpClient();
+
+            var kanyeURL = "https://api.kanye.rest";
+
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine("----");
+            //Console.WriteLine($"Kanye: '{kanyeQuote}'");
+            return kanyeQuote;
+        }
+
+        public static string RonQuote()
+        {
+            var client = new HttpClient();
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            Console.WriteLine("-------");
+            //Console.WriteLine($"Ron: '{ronQuote}'");
+
+            return ronQuote;
+        }
+
+
+
+
     }
+
 }
